5e6429aa45cdf94899d17c4687257f8cf312065c
jsoares <jsoares@a3a5eddf-6203-49af-8a4a-b68c2cc3a614>
2009-01-28 21:36:51 +0000

    Changed all the setting values to reflect the previous changeset
    git-svn-id: http://opensource.washingtontimes.com/projects/public/django-supertagging/trunk@15 a3a5eddf-6203-49af-8a4a-b68c2cc3a614

--- supertagging/settings.py
+++ supertagging/settings.py
@@ -1,19 +1,20 @@
 # These are all the settings that are used to configure supertagging
 
-CALAUS_DEBUG = True
-CALAIS_API_KEY = 'your key here'
-CALAIS_USER_DIRECTIVES = {}
-CALAIS_PROCESING_DIRECTIVES = {}
-CALAIS_EXTERNAL_METADATA = {}
-CALAIS_PROCESS_RELATIONS = True
-CALAIS_PROCESS_TOPICS = True
+SUPERTAGGING_DEBUG = True
+SUPERTAGGING_CALAIS_API_KEY = 'your key here'
+SUPERTAGGING_CALAIS_USER_DIRECTIVES = {}
+SUPERTAGGING_CALAIS_PROCESING_DIRECTIVES = {}
+SUPERTAGGING_CALAIS_EXTERNAL_METADATA = {}
+SUPERTAGGING_PROCESS_RELATIONS = True
+SUPERTAGGING_PROCESS_TOPICS = True
 # IS THIS REALLY NEEDED?
 # The reason this was added was because url get caught by calais, though 
 # they look bad in the list of tags (the full url), we can handle it here
 # or perhaps the query? Should this method exclude the adding of these types
 # or exclude the query from returning these types?
-CALAIS_ENTITY_TYPE_EXCLUSIONS = ['url',]
-CALAIS_MODULES = {
+SUPERTAGGING_TAG_TYPE_EXCLUSIONS = ['url',]
+#SUPERTAGGING_TAG_TYPE_QUERY_EXCLUSIONS = ['url',]
+SUPERTAGGING_MODULES = {
     'app.model': {'fields':({'name': 'content', 'content_type':'TEXT/RAW'},
                             {'name': 'tease',},)}
 }
