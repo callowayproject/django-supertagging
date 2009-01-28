26d4741d7b19e687f82a6a7ab9f522f447847017
jsoares <jsoares@a3a5eddf-6203-49af-8a4a-b68c2cc3a614>
2009-01-28 19:24:21 +0000

    Changed a configuration settings
    git-svn-id: http://opensource.washingtontimes.com/projects/public/django-supertagging/trunk@11 a3a5eddf-6203-49af-8a4a-b68c2cc3a614

--- supertagging/settings.py
+++ supertagging/settings.py
@@ -8,7 +8,11 @@ CALAIS_EXTERNAL_METADATA = {}
 CALAIS_PROCESS_RELATIONS = True
 CALAIS_PROCESS_TOPICS = True
 # IS THIS REALLY NEEDED?
-CALAIS_ENTITY_TYPE_EXCLUSIONS = ['URL',]
+# The reason this was added was because url get caught by calais, though 
+# they look bad in the list of tags (the full url), we can handle it here
+# or perhaps the query? Should this method exclude the adding of these types
+# or exclude the query from returning these types?
+CALAIS_ENTITY_TYPE_EXCLUSIONS = ['url',]
 CALAIS_MODULES = {
     'app.model': {'fields':({'name': 'content', 'content_type':'TEXT/RAW'},
                             {'name': 'tease',},)}
