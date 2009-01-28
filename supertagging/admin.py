03fe5c598853b269c563633580fc036060f43a66
jsoares <jsoares@a3a5eddf-6203-49af-8a4a-b68c2cc3a614>
2009-01-28 21:38:41 +0000

    Added a doc/ folder and various changes to the admin.py file
    git-svn-id: http://opensource.washingtontimes.com/projects/public/django-supertagging/trunk@17 a3a5eddf-6203-49af-8a4a-b68c2cc3a614

--- supertagging/admin.py
+++ supertagging/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
 
 class SuperTagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'stype', 'properties' )
+    list_display = ('name', 'slug', 'stype', 'properties' )
     ordering = ('name', )
     search_fields = ('stype', 'name', )
     list_filter = ('stype', )
     
     
 class SuperTaggedItemAdmin(admin.ModelAdmin):
-    list_display = ('content_object', 'tag', 'field', 'relevance', 'instances')
-    
+    list_display = ('content_object', 'tag', 'field', 'process_type', 'relevance', 'instances')
+    list_filter = ('process_type',)
     
 class SuperTaggedRelationItemAdmin(admin.ModelAdmin):
-    list_display = ('content_object', 'relation', 'field', 'instances')  
+    list_display = ('content_object', 'relation', 'field', 'process_type','instances')  
+    list_filter = ('process_type',)
     
     
 class SuperTagRelationAdmin(admin.ModelAdmin):
