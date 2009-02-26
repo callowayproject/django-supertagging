a45de9867b19d7e65fbdb244c8ffdc7f21c44c40
yml <yml@a3a5eddf-6203-49af-8a4a-b68c2cc3a614>
2009-02-26 20:39:04 +0000

    closes #5. Improve the unicode support. This changeset also change the prerequisite you need to grab python-calais from there https://code.launchpad.net/~yml/python-calais/with_setup
    git-svn-id: http://opensource.washingtontimes.com/projects/public/django-supertagging/trunk@42 a3a5eddf-6203-49af-8a4a-b68c2cc3a614

--- supertagging/modules/__init__.py
+++ supertagging/modules/__init__.py
@@ -15,7 +15,6 @@ except ImportError:
 
 from supertagging import settings
 from supertagging.models import SuperTag, SuperTagRelation, SuperTaggedItem, SuperTaggedRelationItem
-from supertagging.utils import unicode_to_ascii
 
 REF_REGEX = "^http://d.opencalais.com/(?P<key>.*)$"
 
@@ -27,16 +26,16 @@ def process(obj, tags=[]):
         raise ImportError("python-calais module was not found.")
     if not settings.API_KEY:
         raise ValueError('Calais API KEY is missing.')
-        
+
     params = settings.MODULES['%s.%s' % (obj._meta.app_label, obj._meta.module_name)]
-    
+
     process_type = settings.DEFAULT_PROCESS_TYPE
     if 'contentType' in settings.PROCESSING_DIR:
         d_proc_type = proc_dir['contentType']
-        
+
     if 'fields' not in params:
         raise Exception('No "fields" found.')
-        
+
     # Create the instance of Calais and setup the parameters,
     # see open-calais.com for more information about user directives,
     # and processing directives
@@ -44,48 +43,43 @@ def process(obj, tags=[]):
     c.user_directives.update(settings.USER_DIR)
     c.processing_directives.update(settings.PROCESSING_DIR)
     c.processing_directives['contentType'] = process_type
-    
+
     processed_tags = []
     for item in params['fields']:
         d = item.copy()
         field = d.pop('name')
         proc_type = d.pop('process_type', process_type)
-        
-        data = getattr(obj, field)
-        
-        if isinstance(data, unicode):
-            data = unicode_to_ascii(data)
-        else:
-            data = unicode_to_ascii(unicode(data, 'utf-8'))
 
-        #data = force_unicode(getattr(obj, field))
-        
+        data = getattr(obj, field)
+
+        data = force_unicode(getattr(obj, field))
+
         # Analyze the text (data)
         result = c.analyze(data)
-    
+
         # Retrieve the Django content type for the obj
         ctype = ContentType.objects.get_for_model(obj)
         # Remove existing items, this ensures tagged items are updated correctly
         SuperTaggedItem.objects.filter(content_type=ctype, object_id=obj.pk, field=field).delete()
         if settings.PROCESS_RELATIONS:
             SuperTaggedRelationItem.objects.filter(content_type=ctype, object_id=obj.pk, field=field).delete()
-    
+
         entities, relations, topics = [], [], []
         # Process entities, relations and topics
         if hasattr(result, 'entities'):
             entities = _processEntities(field, result.entities, obj, ctype, proc_type, tags)
-        
+
         if hasattr(result, 'relations') and settings.PROCESS_RELATIONS:
             relations = _processRelations(field, result.relations, obj, ctype, proc_type, tags)
-        
+
         if hasattr(result, 'topics') and settings.PROCESS_TOPICS:
             topics =  _processTopics(field, result.topics, obj, ctype, tags)
-        
+
         processed_tags.extend(entities)
         processed_tags.extend(topics)
-        
+
     return processed_tags
-    
+
 def clean_up(obj):
     """
     When an object is removed, remove all the super tagged items and super tagged relation items
@@ -93,10 +87,10 @@ def clean_up(obj):
     cont_type = ContentType.objects.get_for_model(obj)
     SuperTaggedItem.objects.filter(content_type=cont_type, object_id=obj.pk).delete()
     SuperTaggedRelationItem.objects.filter(content_type=cont_type, object_id=obj.pk).delete()
-    
+
     # TODO, clean up tags that have no related items?
     # Same for relations?
-    
+
 def _processEntities(field, data, obj, ctype, process_type, tags):
     """
     Process Entities.
@@ -106,15 +100,23 @@ def _processEntities(field, data, obj, ctype, process_type, tags):
         # Here we convert the given float value to an integer
         rel = int(float(str(entity.pop('relevance', '0'))) * 1000)
         inst = entity.pop('instances', {})
-        
+        ## Tidy up the encoding
+        for i, j in enumerate(inst):
+            for k, v in j.items():
+                if isinstance(v, unicode):
+                    inst[i][k] = v.encode('utf-8')
+                else:
+                    inst[i][k] = v
+
+
         pk = re.match(REF_REGEX, str(entity.pop('__reference'))).group('key')
         stype = entity.pop('_type', '')
-        
+
         if stype.lower() not in settings.EXCLUSIONS:
             name = entity.pop('name', '').lower()
             if tags and name not in tags:
                 continue
-                
+
             slug = slugify(name)
             try:
                 tag = SuperTag.objects.get(name__iexact=name)
@@ -122,13 +124,13 @@ def _processEntities(field, data, obj, ctype, process_type, tags):
                 tag = SuperTag.objects.create(id=pk, slug=slug, stype=stype, name=name)
             except SuperTag.MultipleObjectsReturned:
                 tag = SuperTag.objects.filter(name__iexact=name)[0]
-    
+
             tag.properties = entity
             tag.save()
-            
+
             #TODO: check to make sure that the entity is not already attached
             # to the content object, if it is, just append the instances. This
-            # should elimiate entities returned with different names such as 
+            # should elimiate entities returned with different names such as
             # 'Washington' and 'Washington DC' but same id
             try:
                 it = SuperTaggedItem.objects.get(tag=tag, content_type=ctype, object_id=obj.pk, field=field)
@@ -137,10 +139,10 @@ def _processEntities(field, data, obj, ctype, process_type, tags):
             except SuperTaggedItem.DoesNotExist:
                 # Create the record that will associate content to tags
                 it = SuperTaggedItem.objects.create(tag=tag, content_type=ctype, object_id=obj.pk, field=field, process_type=process_type, relevance=rel, instances=inst)
-    
+
             processed_tags.append(tag)
     return processed_tags
-    
+
 def _processRelations(field, data, obj, ctype, process_type, tags):
     """
     Process Relations
@@ -149,21 +151,21 @@ def _processRelations(field, data, obj, ctype, process_type, tags):
         di.pop('__reference')
         inst = di.pop('instances', {})
         rel_type = di.pop('_type', '')
-        
+
         props = {}
         entities = {}
         # Loop all the items in searh of entities (SuperTags).
         for k,v in di.items():
-            res = re.match(REF_REGEX, str(v))
+            res = re.match(REF_REGEX, unicode(v))
             if res:
                 entities[k] = res.group('key')
             else:
                 props[k] = v
-                
+
         # Make a copy of the found entities
         entities_temp = entities.copy()
         # This double loop builds a properties dict that includes entities as "Text" and not
-        # as the "ID", since the SuperTagRelation needs one "entity" we take one and try to 
+        # as the "ID", since the SuperTagRelation needs one "entity" we take one and try to
         # resolve the other found entities. Entities should already exist from the previous
         # opertaion "_processEntities"
         for entity_key,entity_value in entities.items():
@@ -171,17 +173,17 @@ def _processRelations(field, data, obj, ctype, process_type, tags):
             for entity_temp_key,entity_temp_value in entities_temp.items():
                 if entity_key != entity_temp_key and entity_value != entity_temp_value:
                     _vals[entity_temp_key] = _getEntityText(entity_temp_value)
-                    
+
             _vals.update(props)
-            
+
             if tags and entity_value not in tags:
                 continue
-        
+
             entity = SuperTag.objects.get(pk=entity_value)
             rel_item, rel_created = SuperTagRelation.objects.get_or_create(tag=entity, name=entity_key, stype=rel_type, properties=_vals)
-            
-            SuperTaggedRelationItem.objects.create(relation=rel_item, content_type=ctype, object_id=obj.pk, field=field, process_type=process_type, instances=inst) 
-            
+
+            SuperTaggedRelationItem.objects.create(relation=rel_item, content_type=ctype, object_id=obj.pk, field=field, process_type=process_type, instances=inst)
+
 def _processTopics(field, data, obj, ctype, tags):
     """
     Process Topics, this opertaion is similar to _processEntities, the only
@@ -190,27 +192,27 @@ def _processTopics(field, data, obj, ctype, tags):
     processed_tags = []
     for di in data:
         di.pop('__reference')
-        
+
         pk = re.match(REF_REGEX, str(di.pop('category'))).group('key')
         stype = 'Topic'
         name = di.pop('categoryName', '').lower()
         if tags and name not in tags:
             continue
-            
+
         slug = slugify(name)
         try:
             tag = SuperTag.objects.get(pk=pk)
         except SuperTag.DoesNotExist:
             tag = SuperTag.objects.create(id=pk, slug=slug, stype=stype, name=name)
-            
+
         tag.properties = di
         tag.save()
-        
+
         SuperTaggedItem.objects.create(tag=tag, content_type=ctype, object_id=obj.pk, field=field)
-        
+
         processed_tags.append(tag)
     return processed_tags
-    
+
 def _getEntityText(key):
     """
     Try to resolve the entity given the key
@@ -221,6 +223,5 @@ def _getEntityText(key):
             return r.name
         except SuperTag.DoesNotExist:
             return key
-    
+
     return key
-        
