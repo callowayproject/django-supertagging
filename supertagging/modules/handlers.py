12204f76c93bad7b29c665de08f30d990c2b6db1
yml <yml@a3a5eddf-6203-49af-8a4a-b68c2cc3a614>
2009-03-03 00:16:31 +0000

    closes #9 - Add a conditional statement in the post_save and post_delete signals checking for AUTO_PROCESS. if AUTO_PROCESS is fasle nothing is done.
    git-svn-id: http://opensource.washingtontimes.com/projects/public/django-supertagging/trunk@43 a3a5eddf-6203-49af-8a4a-b68c2cc3a614

--- supertagging/modules/handlers.py
+++ supertagging/modules/handlers.py
@@ -5,22 +5,24 @@ from supertagging import settings
 from supertagging.modules import process, clean_up
 
 def save_handler(sender, **kwargs):
-    try:
-        process(kwargs['instance'])
-    except Exception, e:
-        if settings.ST_DEBUG: raise Exception(e)
-        
+    if settings.AUTO_PROCESS:
+        try:
+            process(kwargs['instance'])
+        except Exception, e:
+            if settings.ST_DEBUG: raise Exception(e)
+
 def delete_handler(sender, **kwargs):
-    try:
-        if 'instance' in kwargs:
-            clean_up(kwargs['instance'])
-    except Exception, e:
-        if settings.ST_DEBUG: raise Exception(e)
-    
+    if settings.AUTO_PROCESS:
+        try:
+            if 'instance' in kwargs:
+                clean_up(kwargs['instance'])
+        except Exception, e:
+            if settings.ST_DEBUG: raise Exception(e)
+
 def setup():
     if not settings.AUTO_PROCESS:
         return
-        
+
     try:
         for k,v in settings.MODULES.items():
             app_label, model_name = k.split('.')
@@ -28,7 +30,6 @@ def setup():
             # Setup post save and post delete handlers
             post_save.connect(save_handler, sender=model)
             post_delete.connect(delete_handler, sender=model)
-            
+
     except Exception, e:
         if settings.ST_DEBUG: raise Exception(e)
-        
