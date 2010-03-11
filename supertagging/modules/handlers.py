ffffc71d62794e20697d1cd2c43cab8ea17e902b
Jose Soares <jsoares@adv1179.nwc.newsworld.net>
2010-03-11 10:49:31 -0500

    Added queue support for the handlers

--- supertagging/modules/handlers.py
+++ supertagging/modules/handlers.py
@@ -2,15 +2,21 @@ from django.db import models
 from django.db.models.signals import post_save, post_delete
 
 from supertagging import settings
-from supertagging.modules import process, clean_up
+from supertagging.modules import process, clean_up, add_to_queue, remove_from_queue
 
 def save_handler(sender, **kwargs):
     if 'instance' in kwargs:
-        process(kwargs['instance'])
+        if settings.USE_QUEUE:
+            add_to_queue(kwargs['instance'])
+        else:
+            process(kwargs['instance'])
 
 def delete_handler(sender, **kwargs):
     if 'instance' in kwargs:
-        clean_up(kwargs['instance'])
+        if settings.USE_QUEUE:
+            remove_from_queue(kwargs['instance'])
+        else:
+            clean_up(kwargs['instance'])
    
 def setup():
     if not settings.AUTO_PROCESS:
@@ -23,6 +29,5 @@ def setup():
             # Setup post save and post delete handlers
             post_save.connect(save_handler, sender=model)
             post_delete.connect(delete_handler, sender=model)
-
     except Exception, e:
         if settings.ST_DEBUG: raise Exception(e)
