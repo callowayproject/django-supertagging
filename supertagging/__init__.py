8b1b7cefe8deb25c7c4f65ea50944de393b3aae4
Corey Oordt <coordt@washingtontimes.com>
2010-06-07 20:07:38 -0400

    Updated the setup.py and version bump to 0.3.2

--- supertagging/__init__.py
+++ supertagging/__init__.py
@@ -1,3 +1,26 @@
-from supertagging.modules import handlers
+__version_info__ = {
+    'major': 0,
+    'minor': 3,
+    'micro': 2,
+    'releaselevel': 'final',
+    'serial': 0
+}
 
-handlers.setup()
+def get_version():
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
+
+    if __version_info__['micro']:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__['releaselevel'] != 'final':
+        vers.append('%(releaselevel)s%(serial)i' % __version_info__)
+    return ''.join(vers)
+
+__version__ = get_version()
+
+
+try:
+    from supertagging.modules import handlers
+    
+    handlers.setup()
+except ImportError:
+    pass
