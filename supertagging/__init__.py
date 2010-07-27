fff4ce038e7e62a4e33283db23cfccfba38073ae
Jose Soares <jsoares@int1665.nwc.newsworld.net>
2010-07-27 10:44:51 -0400

    Added the default register, borrowed from django-tagging

--- supertagging/__init__.py
+++ supertagging/__init__.py
@@ -16,3 +16,48 @@ def get_version():
     return ''.join(vers)
 
 __version__ = get_version()
+
+class AlreadyRegistered(Exception):
+    """
+    An attempt was made to register a model more than once.
+    """
+    pass
+
+
+registry = []
+
+
+def register(model, tag_descriptor_attr='supertags',
+             tagged_item_manager_attr='supertagged'):
+    """
+    Sets the given model class up for working with supertags.
+    """
+
+    from supertagging.managers import ModelTaggedItemManager, TagDescriptor
+
+    if model in registry:
+        raise AlreadyRegistered("The model '%s' has already been "
+            "registered." % model._meta.object_name)
+    if hasattr(model, tag_descriptor_attr):
+        raise AttributeError("'%s' already has an attribute '%s'. You must "
+            "provide a custom tag_descriptor_attr to register." % (
+                model._meta.object_name,
+                tag_descriptor_attr,
+            )
+        )
+    if hasattr(model, tagged_item_manager_attr):
+        raise AttributeError("'%s' already has an attribute '%s'. You must "
+            "provide a custom tagged_item_manager_attr to register." % (
+                model._meta.object_name,
+                tagged_item_manager_attr,
+            )
+        )
+
+    # Add tag descriptor
+    setattr(model, tag_descriptor_attr, TagDescriptor())
+
+    # Add custom manager
+    ModelTaggedItemManager().contribute_to_class(model, tagged_item_manager_attr)
+
+    # Finally register in registry
+    registry.append(model)
