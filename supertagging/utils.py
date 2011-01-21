6127d3b47f3a3e484a52bc7b535881edbc118090
jose soares <jsoares@ashish-sens-computer.local>
2011-01-21 10:41:11 -0500

    Added missing util functions

--- supertagging/utils.py
+++ supertagging/utils.py
@@ -23,6 +23,117 @@ def tag_instance_cmp(x, y):
         return cmp(x['offset'],y['offset'])
     return cmp(1, 1)
 
+def parse_tag_input(input):
+    """
+    Parses tag input, with multiple word input being activated and
+    delineated by commas and double quotes. Quotes take precedence, so
+    they may contain commas.
+
+    Returns a sorted list of unique tag names.
+    """
+    if not input:
+        return []
+
+    input = force_unicode(input)
+
+    # Special case - if there are no commas or double quotes in the
+    # input, we don't *do* a recall... I mean, we know we only need to
+    # split on spaces.
+    if u',' not in input and u'"' not in input:
+        words = list(set(split_strip(input, u' ')))
+        words.sort()
+        return words
+
+    words = []
+    buffer = []
+    # Defer splitting of non-quoted sections until we know if there are
+    # any unquoted commas.
+    to_be_split = []
+    saw_loose_comma = False
+    open_quote = False
+    i = iter(input)
+    try:
+        while 1:
+            c = i.next()
+            if c == u'"':
+                if buffer:
+                    to_be_split.append(u''.join(buffer))
+                    buffer = []
+                # Find the matching quote
+                open_quote = True
+                c = i.next()
+                while c != u'"':
+                    buffer.append(c)
+                    c = i.next()
+                if buffer:
+                    word = u''.join(buffer).strip()
+                    if word:
+                        words.append(word)
+                    buffer = []
+                open_quote = False
+            else:
+                if not saw_loose_comma and c == u',':
+                    saw_loose_comma = True
+                buffer.append(c)
+    except StopIteration:
+        # If we were parsing an open quote which was never closed treat
+        # the buffer as unquoted.
+        if buffer:
+            if open_quote and u',' in buffer:
+                saw_loose_comma = True
+            to_be_split.append(u''.join(buffer))
+    if to_be_split:
+        if saw_loose_comma:
+            delimiter = u','
+        else:
+            delimiter = u' '
+        for chunk in to_be_split:
+            words.extend(split_strip(chunk, delimiter))
+    words = list(set(words))
+    words.sort()
+    return words
+    
+def split_strip(input, delimiter=u','):
+    """
+    Splits ``input`` on ``delimiter``, stripping each resulting string
+    and returning a list of non-empty strings.
+    """
+    if not input:
+        return []
+
+    words = [w.strip() for w in input.split(delimiter)]
+    return [w for w in words if w]
+
+def edit_string_for_tags(tags):
+    """
+    Given list of ``SuperTag`` instances, creates a string representation of
+    the list suitable for editing by the user, such that submitting the
+    given string representation back without changing it will give the
+    same list of tags.
+
+    Tag names which contain commas will be double quoted.
+
+    If any tag name which isn't being quoted contains whitespace, the
+    resulting string of tag names will be comma-delimited, otherwise
+    it will be space-delimited.
+    """
+    names = []
+    use_commas = False
+    for tag in tags:
+        name = tag.name
+        if u',' in name:
+            names.append('"%s"' % name)
+            continue
+        elif u' ' in name:
+            if not use_commas:
+                use_commas = True
+        names.append(name)
+    if use_commas:
+        glue = u', '
+    else:
+        glue = u' '
+    return glue.join(names)
+
 def get_queryset_and_model(queryset_or_model):
     """
     Given a ``QuerySet`` or a ``Model``, returns a two-tuple of
@@ -40,11 +151,11 @@ def get_tag_list(tags):
     """
     Utility function for accepting tag input in a flexible manner.
 
-    If a ``Tag`` object is given, it will be returned in a list as
+    If a ``SuperTag`` object is given, it will be returned in a list as
     its single occupant.
 
     If given, the tag names in the following will be used to create a
-    ``Tag`` ``QuerySet``:
+    ``SuperTag`` ``QuerySet``:
 
        * A string, which may contain multiple tag names.
        * A list or tuple of strings corresponding to tag names.
@@ -52,8 +163,8 @@ def get_tag_list(tags):
 
     If given, the following will be returned as-is:
 
-       * A list or tuple of ``Tag`` objects.
-       * A ``Tag`` ``QuerySet``.
+       * A list or tuple of ``SuperTag`` objects.
+       * A ``SuperTag`` ``QuerySet``.
 
     """
     from supertagging.models import SuperTag
@@ -84,7 +195,7 @@ def get_tag_list(tags):
             elif 'int' in contents:
                 return SuperTag.objects.filter(id__in=tags)
         else:
-            raise ValueError(_('If a list or tuple of tags is provided, they must all be tag names, Tag objects or Tag ids.'))
+            raise ValueError(_('If a list or tuple of tags is provided, they must all be tag names, SuperTag objects or Tag ids.'))
     else:
         raise ValueError(_('The tag input given was invalid.'))
 
