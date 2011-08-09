da4aaa8c0339181fb92349678fc89ba28e0500cd
Corey Oordt <coordt@washingtontimes.com>
2011-08-09 09:03:32 -0400

    Including flat pages for testing purposes.

--- example/settings.py
+++ example/settings.py
@@ -93,6 +93,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
