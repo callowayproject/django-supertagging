63373075e4e363ce0feb7391ce0a61c4b466ddd5
Corey Oordt <coreyoordt@gmail.com>
2013-03-24 12:07:48 -0500

    Forgot a file for PEP 8 cleaning

--- supertagging/templatetags/supertagging_tags.py
+++ supertagging/templatetags/supertagging_tags.py
@@ -1,29 +1,24 @@
 # Most template tags were borrowed from django-tagging.
 
-import django
-from django.db.models import get_model
-from django.template import (Library, Node, TemplateSyntaxError, Variable, 
-                            resolve_variable, VariableDoesNotExist)
-from django.utils.translation import ugettext as _
+from django.contrib.admin.templatetags.admin_list import _boolean_icon, DOT
+from django.contrib.admin.util import lookup_field, display_for_field
 from django.contrib.admin.views.main import (ALL_VAR, EMPTY_CHANGELIST_VALUE,
-    ORDER_VAR, ORDER_TYPE_VAR, PAGE_VAR, SEARCH_VAR)
+                                             ORDER_VAR)
 from django.core.exceptions import ObjectDoesNotExist
-try:
-    from django.contrib.admin.util import (lookup_field, display_for_field, 
-                                            label_for_field)
-except ImportError:
-    from supertagging.utils import (lookup_field, display_for_field, 
-                                    label_for_field)
-from django.utils.safestring import mark_safe
+from django.db import models
+from django.db.models import get_model
+from django.template import Library, Node, TemplateSyntaxError, Variable
 from django.utils.encoding import smart_unicode, force_unicode
 from django.utils.html import escape, conditional_escape
-from django.db import models
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
 
-from supertagging.models import (SuperTag, SuperTaggedItem, SuperTagRelation, 
+from supertagging.models import (SuperTag, SuperTaggedItem, SuperTagRelation,
                                  SuperTaggedRelationItem)
 from supertagging.utils import LINEAR, LOGARITHMIC
 
 register = Library()
+
 
 class TagsForModelNode(Node):
     def __init__(self, model, context_var, counts, **kwargs):
@@ -36,9 +31,9 @@ class TagsForModelNode(Node):
         model = get_model(*self.model.split('.'))
         if model is None:
             raise TemplateSyntaxError(_('supertags_for_model tag was given an invalid model: %s') % self.model)
-        
+
         if 'filters' in self.kwargs and isinstance(self.kwargs['filters'], dict):
-            for k,v in self.kwargs['filters'].items():
+            for k, v in self.kwargs['filters'].items():
                 try:
                     v = Variable(v).resolve(context)
                     self.kwargs['filters'][k] = v
@@ -46,6 +41,7 @@ class TagsForModelNode(Node):
                     continue
         context[self.context_var] = SuperTag.objects.usage_for_model(model, counts=self.counts)
         return ''
+
 
 class TagCloudForModelNode(Node):
     def __init__(self, model, context_var, **kwargs):
@@ -57,9 +53,9 @@ class TagCloudForModelNode(Node):
         model = get_model(*self.model.split('.'))
         if model is None:
             raise TemplateSyntaxError(_('supertag_cloud_for_model tag was given an invalid model: %s') % self.model)
-        
+
         if 'filters' in self.kwargs and isinstance(self.kwargs['filters'], dict):
-            for k,v in self.kwargs['filters'].items():
+            for k, v in self.kwargs['filters'].items():
                 try:
                     v = Variable(v).resolve(context)
                     self.kwargs['filters'][k] = v
@@ -68,6 +64,7 @@ class TagCloudForModelNode(Node):
         context[self.context_var] = \
             SuperTag.objects.cloud_for_model(model, **self.kwargs)
         return ''
+
 
 class TagsForObjectNode(Node):
     def __init__(self, obj, context_var, **kwargs):
@@ -79,6 +76,7 @@ class TagsForObjectNode(Node):
         context[self.context_var] = \
             SuperTag.objects.get_for_object(self.obj.resolve(context), **self.kwargs)
         return ''
+
 
 class TaggedObjectsNode(Node):
     def __init__(self, tag, model, context_var):
@@ -94,6 +92,7 @@ class TaggedObjectsNode(Node):
             SuperTaggedItem.objects.get_by_model(model, self.tag.resolve(context))
         return ''
 
+
 class RelatedObjectsForObjectNode(Node):
     def __init__(self, obj, model, context_var, **kwargs):
         self.obj = Variable(obj)
@@ -108,6 +107,7 @@ class RelatedObjectsForObjectNode(Node):
         context[self.context_var] = \
             SuperTaggedItem.objects.get_related(self.obj.resolve(context), model, **self.kwargs)
         return ''
+
 
 def do_tags_for_model(parser, token):
     """
@@ -132,13 +132,14 @@ def do_tags_for_model(parser, token):
 
        {% supertags_for_model products.Widget as widget_tags %}
        {% supertags_for_model products.Widget as widget_tags with counts %}
-       
+
        {% supertags_for_model products.Widget as widget_tags with counts product__category__pk=1 %}
        {% supertags_for_model products.Widget as widget_tags with counts product__category__pk=category.pk %}
 
     """
     bits = token.contents.split()
     len_bits = len(bits)
+    kwargs = {}
     if not len_bits > 3:
         raise TemplateSyntaxError(_('%s tag requires more than two arguments') % bits[0])
     if bits[2] != 'as':
@@ -159,7 +160,7 @@ def do_tags_for_model(parser, token):
                 else:
                     # The remaining bits should be consider extra query params
                     kwargs['filters'][name] = value
-                    
+
             except ValueError:
                 raise TemplateSyntaxError(_("%(tag)s tag was given a badly formatted option: '%(option)s'") % {
                     'tag': bits[0],
@@ -170,9 +171,10 @@ def do_tags_for_model(parser, token):
         if bits[5] != 'counts':
             raise TemplateSyntaxError(_("if given, fifth argument to %s tag must be 'counts'") % bits[0])
     if len_bits == 4:
-        return TagsForModelNode(bits[1], bits[3], counts=False)
+        return TagsForModelNode(bits[1], bits[3], counts=False, **kwargs)
     else:
-        return TagsForModelNode(bits[1], bits[3], counts=True)
+        return TagsForModelNode(bits[1], bits[3], counts=True, **kwargs)
+
 
 def do_tag_cloud_for_model(parser, token):
     """
@@ -203,28 +205,31 @@ def do_tag_cloud_for_model(parser, token):
        ``distribution``
           One of ``linear`` or ``log``. Defines the font-size
           distribution algorithm to use when generating the tag cloud.
-          
-        If anything else is speficied in the options, it will be consider 
+
+        If anything else is speficied in the options, it will be consider
         extra filter params
 
     Examples::
 
        {% supertag_cloud_for_model products.Widget as widget_tags %}
        {% supertag_cloud_for_model products.Widget as widget_tags with steps=9 min_count=3 distribution=log %}
-       
+
        {% supertag_cloud_for_model products.Widget as widget_tags with steps=9 min_count=3 distrubution=log product__category__pk=1 %}
        {% supertag_cloud_for_model products.Widget as widget_tags with steps=9 min_count=3 distrubution=log product__category__pk=category.pk %}
     """
     bits = token.contents.split()
     len_bits = len(bits)
     if not len_bits > 3:
-        raise TemplateSyntaxError(_('%s tag requires more than two arguments') % bits[0])
+        raise TemplateSyntaxError(
+            _('%s tag requires more than two arguments') % bits[0])
     if bits[2] != 'as':
-        raise TemplateSyntaxError(_("second argument to %s tag must be 'as'") % bits[0])
+        raise TemplateSyntaxError(
+            _("second argument to %s tag must be 'as'") % bits[0])
     kwargs = {'filters': {}}
     if len_bits > 5:
         if bits[4] != 'with':
-            raise TemplateSyntaxError(_("if given, fourth argument to %s tag must be 'with'") % bits[0])
+            raise TemplateSyntaxError(
+                _("if given, fourth argument to %s tag must be 'with'") % bits[0])
         for i in range(5, len_bits):
             try:
                 name, value = bits[i].split('=')
@@ -249,13 +254,14 @@ def do_tag_cloud_for_model(parser, token):
                 else:
                     # The remaining bits should be consider extra query params
                     kwargs['filters'][name] = value
-                    
+
             except ValueError:
                 raise TemplateSyntaxError(_("%(tag)s tag was given a badly formatted option: '%(option)s'") % {
                     'tag': bits[0],
                     'option': bits[i],
                 })
     return TagCloudForModelNode(bits[1], bits[3], **kwargs)
+
 
 def do_tags_for_object(parser, token):
     """
@@ -269,9 +275,9 @@ def do_tags_for_object(parser, token):
     Example::
 
         {% supertags_for_object foo_object as tag_list %}
-        
+
         {% supertags_for_object foo_object as tag_list with field=story %}
-        
+
     """
     bits = token.contents.split()
     len_bits = len(bits)
@@ -302,6 +308,7 @@ def do_tags_for_object(parser, token):
                 })
     return TagsForObjectNode(bits[1], bits[3], **kwargs)
 
+
 def do_tagged_objects(parser, token):
     """
     Retrieves a list of instances of a given model which are tagged with
@@ -329,6 +336,7 @@ def do_tagged_objects(parser, token):
         raise TemplateSyntaxError(_("fourth argument to %s tag must be 'as'") % bits[0])
     return TaggedObjectsNode(bits[1], bits[3], bits[5])
 
+
 def do_related_objects_for_object(parser, token):
     """
     Retrieves a list of related objects of a given model which shares tags
@@ -345,7 +353,7 @@ def do_related_objects_for_object(parser, token):
     Example::
 
         {% related_objects_for_object show in tv.Show as related_objects %}
-        
+
         {% related_objects_for_object show in tv.Show as related_objects with min_relevance=500 %}
 
     """
@@ -406,8 +414,8 @@ class RelationsForTagNode(Node):
             SuperTagRelation.objects.get_for_tag(
                 self.obj.resolve(context), **kwargs)
         return ''
-        
-        
+
+
 class RelationsForObjectNode(Node):
     def __init__(self, obj, context_var, stype=None):
         self.obj = Variable(obj)
@@ -422,8 +430,8 @@ class RelationsForObjectNode(Node):
             SuperTaggedRelationItem.objects.get_for_object(
                 self.obj.resolve(context), **kwargs)
         return ''
-        
-        
+
+
 class RelationsForTagInObjectNode(Node):
     def __init__(self, tag, obj, context_var):
         self.obj = Variable(obj)
@@ -459,61 +467,62 @@ def do_relations_for_tag(parser, token):
         raise TemplateSyntaxError(_('%s tag requires at least three arguments and at most six arguments') % bits[0])
     if bits[2] != 'as':
         raise TemplateSyntaxError(_("Second argument for %s tag must be 'as'") % bits[0])
-        
+
     if len(bits) > 4:
         if bits[4] != "with":
-            raise TemplateSyntacError(_("Fouth argument for %s tag must be 'with'") % bits[0])
-        
+            raise TemplateSyntaxError(_("Fouth argument for %s tag must be 'with'") % bits[0])
+
         if len(bits[5].split("=")) == 2 and bits[5].split("=")[0] == 'type':
-             return RelationsForTagNode(bits[1], bits[3], bits[5].split("=")[1])
+            return RelationsForTagNode(bits[1], bits[3], bits[5].split("=")[1])
         else:
-            raise TemplateSyntacError(_("Last argument for %s tag must be in the format type=[TYPE]") % bits[0])
+            raise TemplateSyntaxError(_("Last argument for %s tag must be in the format type=[TYPE]") % bits[0])
     return RelationsForTagNode(bits[1], bits[3])
-  
+
 
 def do_relations_for_object(parser, token):
     """
     Retrieves a list of ``Relations`` for a given object
-    
+
     Useage::
-        
+
         {% relations_for_object [object] as [varname] %}
         {% relations_for_object [object] as [varname] with [type=TYPE]}
-        
+
     Example::
-    
+
         {% relations_for_object story as story_relations %}
         {% relations_for_object story as story_relations with type=Quotation %}
-        
+
     """
     bits = token.contents.split()
     if len(bits) < 4 or len(bits) > 6:
         raise TemplateSyntaxError(_('%s tag requires at least three arguments and at most six arguments') % bits[0])
     if bits[2] != 'as':
         raise TemplateSyntaxError(_("Second argument for %s tag must be 'as'") % bits[0])
-        
+
     if len(bits) > 4:
         if bits[4] != "with":
-            raise TemplateSyntacError(_("Fouth argument for %s tag must be 'with'") % bits[0])
-        
+            raise TemplateSyntaxError(_("Fouth argument for %s tag must be 'with'") % bits[0])
+
         if len(bits[5].split("=")) == 2 and bits[5].split("=")[0] == 'type':
-             return RelationsForObjectNode(bits[1], bits[3], bits[5].split("=")[1])
+            return RelationsForObjectNode(bits[1], bits[3], bits[5].split("=")[1])
         else:
-            raise TemplateSyntacError(_("Last argument for %s tag must be in the format type=[TYPE]") % bits[0])
+            raise TemplateSyntaxError(_("Last argument for %s tag must be in the format type=[TYPE]") % bits[0])
     return RelationsForObjectNode(bits[1], bits[3])
-    
+
+
 def do_relations_for_tag_in_object(parser, token):
     """
     Retrieves a list of ``Relations`` for a given tag that is also in object
-    
+
     Useage::
-        
+
         {% relations_for [tag] in [object] as [varname] %}
-        
+
     Example::
-    
+
         {% relations_for state_tag in obj as obj_relations %}
-        
+
     """
     bits = token.contents.split()
     if len(bits) != 6:
@@ -523,10 +532,7 @@ def do_relations_for_tag_in_object(parser, token):
     if bits[4] != 'as':
         raise TemplateSyntaxError(_("Second argument to %s tag must be 'as'") % bits[0])
     return RelationsForTagInObjectNode(bits[1], bits[3], bits[5])
- 
 
-    
-    
 register.tag('relations_for_supertag', do_relations_for_tag)
 register.tag('relations_for_object', do_relations_for_object)
 register.tag('relations_for', do_relations_for_tag_in_object)
@@ -537,7 +543,7 @@ class RenderItemNode(Node):
         self.obj = obj
         self.template = template
         self.suffix = suffix
-        
+
     def render(self, context):
         suffix, template = self.suffix, self.template
         try:
@@ -547,74 +553,76 @@ class RenderItemNode(Node):
                 if isinstance(obj, c):
                     isinst = True
                     break
-                    
+
             if not isinst:
                 return None
         except:
             return None
-            
+
         return obj.render(template=template, suffix=suffix)
-        
-        
+
+
 def do_render_item(parser, token):
     """
     {% supertag_render [SuperTag or SuperTaggedItem or SuperTagRelation or SuperTaggedRelationItem] [with] [suffix=S] [template=T] %}
     {% supertag_render tag %}
     {% supertag_render tagged_item with suffix=custom %}
     {% supertag_render rel_item with template=mycustomtemplates/supertags/custom.html %}
-    
+
     Only suffix OR template can be specified, but not both.
     """
     argv = token.contents.split()
     argc = len(argv)
-    
+
     if argc < 2 or argc > 4:
-        raise TemplateSyntaxError, "Tag %s takes either two or four arguments." % argv[0]
-        
+        raise TemplateSyntaxError("Tag %s takes either two or four arguments." % argv[0])
+
     if argc == 2:
         return RenderItemNode(argv[1])
     else:
         if argv[2] != 'with':
-            raise TemplateSyntaxError, 'Second argument must be "with" for tag %s.' % argv[0]
+            raise TemplateSyntaxError('Second argument must be "with" for tag %s.' % argv[0])
         extra = argv[3].split('=')
         if len(extra) != 2:
-            raise TemplateSyntaxError, "Last argument must be formated correctly for tag %s." % argv[0]
+            raise TemplateSyntaxError("Last argument must be formated correctly for tag %s." % argv[0])
         if not extra[0] in ['suffix', 'template']:
-            raise TemplateSyntaxError, "Last argment must of either suffix or template for tag %s." % argv[0]
-            
-        kwargs = {str(extra[0]): extra[1],}
+            raise TemplateSyntaxError("Last argment must of either suffix or template for tag %s." % argv[0])
+
+        kwargs = {str(extra[0]): extra[1], }
         return RenderItemNode(argv[1], **kwargs)
-        
+
 register.tag('supertag_render', do_render_item)
 
 
 class IsSupertaggableNode(Node):
     def __init__(self, obj):
         self.obj = obj
-    
+
     def render(self, context):
         from django.contrib.contenttypes.models import ContentType
         from supertagging.settings import WATCHED_FIELDS
-        
+
         obj = Variable(self.obj).resolve(context)
         ctype = ContentType.objects.get_for_model(obj)
         result = "%s.%s" % (ctype.app_label, ctype.model) in WATCHED_FIELDS
         context['is_supertaggable'] = result
         return ''
 
+
 def do_is_supertaggable(parser, token):
     """
     {% is_supertaggable obj %}
-    
+
     Sets context variable "is_supertaggable" to True or False
     """
     try:
         tag_name, obj = token.split_contents()
     except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires exactly one argument" % token.contents.split()[0])
+        raise TemplateSyntaxError("%r tag requires exactly one argument" % token.contents.split()[0])
     return IsSupertaggableNode(obj)
 
 register.tag('is_supertaggable', do_is_supertaggable)
+
 
 def items_for_result(cl, result, form):
     """
@@ -662,9 +670,7 @@ def items_for_result(cl, result, form):
             result_repr = mark_safe('&nbsp;')
         # If list_display_links not defined, add the link tag to the first field
         if (first and not cl.list_display_links) or field_name in cl.list_display_links:
-            table_tag = {True:'th', False:'td'}[first]
-            if form and django.VERSION[1] == 1: # Django 1.1 
-                result_repr += force_unicode(form[cl.model._meta.pk.name])
+            table_tag = {True: 'th', False: 'td'}[first]
             first = False
             # Convert the pk to something that can be used in Javascript.
             # Problem cases are long ints (23L) and non-ASCII strings.
@@ -709,6 +715,7 @@ def results(cl):
             for res in cl.result_list:
                 yield list(items_for_result(cl, res, None))
 
+
 @register.inclusion_tag("admin/supertagging/supertaggeditem/change_list_results.html")
 def supertaggeditem_result_list(cl):
     """
@@ -719,7 +726,7 @@ def supertaggeditem_result_list(cl):
         from django.contrib.admin.templatetags.admin_list import result_hidden_fields
     except ImportError:
         result_hidden_fields = lambda x: []
-    
+
     headers = list(result_headers(cl))
     cl.list_editable = cl.model_admin.list_editable
     return {'cl': cl,
@@ -727,6 +734,7 @@ def supertaggeditem_result_list(cl):
             'result_headers': headers,
             'reset_sorting_url': cl.get_query_string(remove=[ORDER_VAR]),
             'results': list(results(cl))}
+
 
 @register.inclusion_tag("admin/supertagging/supertaggeditem/pagination.html")
 def supertaggeditem_pagination(cl):
