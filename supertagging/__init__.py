a1dbf374e0015e7106507dbdc021694e37636116
Corey Oordt <coreyoordt@gmail.com>
2013-04-22 19:30:48 -0500

    Updating the get_version method

--- supertagging/__init__.py
+++ supertagging/__init__.py
@@ -7,13 +7,13 @@ __version_info__ = {
 }
 
 
-def get_version():
+def get_version(short=False):
+    assert __version_info__['releaselevel'] in ('alpha', 'beta', 'final')
     vers = ["%(major)i.%(minor)i" % __version_info__, ]
-
-    if __version_info__['micro']:
+    if __version_info__['micro'] and not short:
         vers.append(".%(micro)i" % __version_info__)
-    if __version_info__['releaselevel'] != 'final':
-        vers.append('%(releaselevel)s%(serial)i' % __version_info__)
+    if __version_info__['releaselevel'] != 'final' and not short:
+        vers.append('%s%i' % (__version_info__['releaselevel'][0], __version_info__['serial']))
     return ''.join(vers)
 
 __version__ = get_version()
